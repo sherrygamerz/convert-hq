b6808ad1eb44e59c62016d640c18ca8800bedc9c
sherrygamerz <111529472+sherrygamerz@users.noreply.github.com>
2025-05-17 20:32:58 +0800

    Update App.py

--- App.py
+++ App.py
@@ -1,21 +1,30 @@
 from flask import Flask, render_template, request, send_file
 from PIL import Image
+import io
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 @app.route('/convert', methods=['POST'])
 def convert():
-    image = request.files['image']
+    image_file = request.files['image']
     format = request.form['format']
-    output_path = f"converted.{format.lower()}"
-    img = Image.open(image)
-    img.save(output_path)
-    return send_file(output_path, as_attachment=True)
+
+    if not image_file:
+        return "No image uploaded!", 400
+
+    img = Image.open(image_file.stream)
+    img_converted = io.BytesIO()
+    img.save(img_converted, format=format)
+    img_converted.seek(0)
+
+    download_filename = f"converted.{format.lower()}"
+    return send_file(img_converted, as_attachment=True, download_name=download_filename, mimetype=f'image/{format.lower()}')
 
 if __name__ == '__main__':
-    app.run() 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
