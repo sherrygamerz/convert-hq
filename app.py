dba1b285ee0f2569dd485e4e8986c7157638b09c
sherrygamerz <111529472+sherrygamerz@users.noreply.github.com>
2025-05-17 21:49:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,6 @@ def convert():
     return send_file(zip_buffer, as_attachment=True, download_name="converted_images.zip", mimetype='application/zip')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
